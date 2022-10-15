0e47fdf9cf14bc1fa1bd8405127bedb80c06db93
y30n9ju1v <iam.yeongjunkim@gmail.com>
2022-10-15 09:24:24 +0900

    update newton method

--- Newton_method.cpp
+++ Newton_method.cpp
@@ -1,52 +1,52 @@
 #include <iostream>
 
-template <typename T, typename U>
-auto average(T x, U y)
-{
-	return (x + y) / 2.0;
-}
-
-template <typename T, typename U>
-auto improve(T guess, U x)
-{
-	return average(guess, (x / guess));
-}
-
 template <typename T>
-auto _square(T x)
+inline auto _square(T x)
 {
 	return x * x;
 }
 
 template <typename T>
-auto _abs(T x)
+inline auto _abs(T x)
 {
 	return (x < 0) ? -x : x;
 }
 
 template <typename T, typename U>
-auto goodEnough(T guess, U x)
+inline auto _average(T x, U y)
+{
+	return (x + y) / 2.0;
+}
+
+template <typename T, typename U>
+inline auto _improve(T guess, U x)
+{
+	return _average(guess, (x / guess));
+}
+
+template <typename T, typename U>
+inline bool _goodEnough(T guess, U x)
 {
 	return (_abs(_square(guess) - x) < 0.001) ? true : false;
 }
 
 template <typename T, typename U>
-auto sqrtIter(T guess, U x)
+inline auto _sqrtIter(T guess, U x)
 {
-	if (goodEnough(guess, x) == true)
+	if (_goodEnough(guess, x) == true)
 	{
 		return guess;
 	}
 	else
 	{
-		sqrtIter(improve(guess, x), x);
+		_sqrtIter(_improve(guess, x), x);
 	}
 }
 
 template <typename T>
 auto _sqrt(T x)
 {
-	return sqrtIter(1.0, x);
+	return _sqrtIter(1.0, x);
 }
 
 int main()
